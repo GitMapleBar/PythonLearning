2f284dc9bf57ba1b808d65cdeea802a07ec3a5b9
Dan <104011874+GitMapleBar@users.noreply.github.com>
2023-03-11 10:11:13 -0800

    Add files via upload

---
+++ 04-16 list_twice.py
@@ -0,0 +1,11 @@
+list = []
+while True:
+    new_item = int(input("New item: "))
+    if new_item == 0:
+        break
+    list.append(new_item)
+    print(f"The list now: {list}")
+    print(f"The list in order: {sorted(list)}")
+print("Bye!")
+
+    
